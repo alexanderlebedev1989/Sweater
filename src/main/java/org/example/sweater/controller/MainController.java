aec8e0537127071b71a7240f21236ca772ff2892
a.lebedev <a.lebedev@isimplelab.com>
2021-06-22 17:15:08 +0300

    подключил шаблонизатор Freemarker

--- src/main/java/org/example/sweater/controller/MainController.java
+++ src/main/java/org/example/sweater/controller/MainController.java
@@ -6,6 +6,7 @@ import org.example.sweater.domain.User;
 import org.example.sweater.repo.MessageRepo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,14 +21,22 @@ public class MainController {
     private final MessageRepo repo;
 
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
+    public String greeting() {
         return "greeting";
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        Iterable<Message> messages = repo.findAll();
-        model.put("messages", messages);
+    public String main(@RequestParam(required = false, defaultValue = "") String tag,
+                       Model model) {
+        Iterable<Message> messages;
+
+        if (StringUtils.hasLength(tag)) {
+            messages = repo.findByTag(tag);
+        } else {
+            messages = repo.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("tag", tag);
         return "main";
     }
 
@@ -35,24 +44,12 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model) {
+            @RequestParam String tag, Model model) {
         Message message = new Message(text, tag, user);
         repo.save(message);
 
         Iterable<Message> messages = repo.findAll();
-        model.put("messages", messages);
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String tag, Map<String, Object> model) {
-        Iterable<Message> messages;
-        if (StringUtils.hasLength(tag)) {
-            messages = repo.findByTag(tag);
-        } else {
-            messages = repo.findAll();
-        }
-        model.put("messages", messages);
+        model.addAttribute("messages", messages);
         return "main";
     }
 }
